305f8e579cc587f1bf8b37831617a82738ff05a9
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-04-26 23:39:09 +0200

    Reorganize examples

---
+++ examples/levels.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var Acho = require('..')
+var acho = Acho({
+  upperCase: true
+})
+
+var levels = Object.keys(acho.types)
+
+var fixtureObj = {
+  foo: 'bar',
+  hello: 'world'
+}
+
+var fixtureArr = [1, 2, 3, 4, 5]
+
+acho.debug('%j', fixtureObj)
+acho.debug(fixtureObj)
+acho.debug(fixtureArr)
+
+levels.forEach(function (level) {
+  setInterval(function () {
+    acho[level]('This is a auto-generated ' + level + ' message')
+    acho[level](fixtureObj)
+  }, 1000)
+})
