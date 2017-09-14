9efff8d9d7c95bc0d8902a7b16c2edf0c898c704
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-09-14 23:38:55 +0200

    Add skin cli example

---
+++ examples/skin-cli.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const acho = require('acho')
+const skinCli = require('acho-skin-cli')
+
+const log = acho({
+  types: skinCli,
+  keyword: 'symbol'
+})
+
+const types = Object.keys(log.types)
+types.forEach(type => {
+  log[type]('hello world')
+})
